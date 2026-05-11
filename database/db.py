81e442a30c08e198c9244d6807b2f39c7bc7568f
Mahammadhanzla <161584945+hanzla-mansuri@users.noreply.github.com>
2026-05-11 13:56:45 +0300

    add login functionality

--- database/db.py
+++ database/db.py
@@ -74,6 +74,15 @@ def seed_db():
     conn.close()
 
 
+def get_user_by_email(email):
+    conn = get_db()
+    user = conn.execute(
+        "SELECT * FROM users WHERE email = ?", (email,)
+    ).fetchone()
+    conn.close()
+    return user
+
+
 def create_user(name, email, password):
     conn = get_db()
     try:
